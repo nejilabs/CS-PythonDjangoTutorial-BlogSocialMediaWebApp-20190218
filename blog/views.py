e67a1f31b2d7dd49e77842781581f1e3d5097299
nellyXinwei <nelsonalbajr@yahoo.com>
2020-07-09 01:35:49 +0800

    new changes

--- blog/views.py
+++ blog/views.py
@@ -12,14 +12,6 @@ from django.views.generic import (
 from .models import Post
 
 
-def home(request):
-    context = {
-        'posts': Post.objects.all(),
-        'title': 'Home'
-    }
-    return render(request,'blog/home.html',context)
-
-
 def about(request):
     return render(request,'blog/about.html', {'title': 'About'})
 
