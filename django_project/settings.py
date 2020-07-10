4f1c53a358a3483e3d510c014368f090fa3cb124
nellyXinwei <nelsonalbajr@yahoo.com>
2020-07-11 01:15:27 +0800

    settings

--- django_project/settings.py
+++ django_project/settings.py
@@ -140,8 +140,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER='nb3.321132@gmail.com' #os.environ.get('EMAIL_USER')
-EMAIL_HOST_PASSWORD='pedwnhbeqvgigisq'#os.environ.get('EMAIL_PASS')
+EMAIL_HOST_USER=os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD=os.environ.get('EMAIL_PASS')
 
 AWS_ACCESS_KEY_ID="AKIAYLD2YRTN36WCNOWR" #os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY="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" #os.environ.get('AWS_SECRET_ACCESS_KEY')
